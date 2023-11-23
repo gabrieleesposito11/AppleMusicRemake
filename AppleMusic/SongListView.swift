869dabaab2eb24e4e3ccfe4d03e17ddbb1ba8d4f
gabrieleesposito11 <gabriele.esposito11@studenti.unina.it>
2023-11-23 12:54:56 +0100

    commit 23 novembre

---
+++ AppleMusic/SongListView.swift
@@ -0,0 +1,18 @@
+//
+//  SongListView.swift
+//  AppleMusic
+//
+//  Created by Gabriele Esposito on 22/11/23.
+//
+
+import SwiftUI
+
+struct SongListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongListView()
+}
